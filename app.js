987de2cb082ff5bf23d16af4da9c62cc14b5e725
KaurTamm <kaur.tamm@khk.ee>
2019-10-23 16:18:25 +0300

    Data Types

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('JS test');
+// Andmetüübid
+
+// Täisarvud
+console.log(10);
+console.log(-4);
+
+// Reaalarvud - ujukomaarvud
+console.log(2.5);
+console.log(0.20);
+
+// Sõned - tekst
+console.log('Hello world');
+
+// Tõeväärtus
+console.log(true);
+console.log(false);
+
+// Nimekirjad
+console.log([1, 2, 3, 4 , 5]);
+
+// Objektid
+console.log({a : 1, b : 2});
+console.table({a : 1, b : 2});
