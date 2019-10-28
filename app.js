aa541d16d064b9e8e061700e1d2d32fdf9d6b3dd
KaurTamm <kaur.tamm@khk.ee>
2019-10-28 11:44:08 +0200

    While

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
 //Tsüklid
 
-let numbrid = [];
-
-for(let i = 0;  i < 10; i++){
-    let arv = Math.floor(Math.random() * 10);
-    numbrid.push(arv);
+//while
+let number = 1;
+while(number <= 10){
+    console.log(number);
+    number++;
 }
-console.log(numbrid);
-
-for(let kord = 0; kord < numbrid.length; kord++){
-    for(let i = 0; i < numbrid.length; i++){
-        let abi;
-        if(numbrid[i] < numbrid[i+1]){
-            abi = numbrid[i];
-            numbrid[i] = numbrid[i+1];
-            numbrid[i+1] = abi;
-        }
-    }
-}
-console.log(numbrid)
