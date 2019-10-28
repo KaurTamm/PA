d4bb1a2f8847caa47fde58b7b8e7975f133585b2
KaurTamm <kaur.tamm@khk.ee>
2019-10-28 11:50:18 +0200

    More while

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 //Tsüklid
 
 //while
-let number = 1;
-while(number <= 10){
-    console.log(number);
-    number++;
+let number = 5;
+let jagaja = 2;
+
+while(number % jagaja != 0){
+    console.log(number % jagaja);
+    jagaja++;
+}
+
+if(number == jagaja){
+    console.log(number + ' on algarv');
+} else{
+    console.log(number+ ' ei ole algarv');
 }
