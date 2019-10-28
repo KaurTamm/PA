eec2bbce08ef0130d222e27e0a3506d431ceb8e7
KaurTamm <kaur.tamm@khk.ee>
2019-10-28 11:34:00 +0200

    Sorting

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 //Tsüklid
 
-let autod = ['Kia, Toyota, Audi, BMW, Ford, Nissan'];
+let numbrid = [];
 
-for(let i = 0; i < autod.length; i++){
-    console.log(autod[i]);
+for(let i = 0;  i < 10; i++){
+    let arv = Math.floor(Math.random() * 10);
+    numbrid.push(arv);
 }
+console.log(numbrid);
+
+for(let kord = 0; kord < numbrid.length; kord++){
+    for(let i = 0; i < numbrid.length; i++){
+        let abi;
+        if(numbrid[i] < numbrid[i+1]){
+            abi = numbrid[i];
+            numbrid[i] = numbrid[i+1];
+            numbrid[i+1] = abi;
+        }
+    }
+}
+console.log(numbrid)
