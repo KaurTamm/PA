d518c7026bf19ae36fb43328b25b5b2b4e3e27b4
KaurTamm <kaur.tamm@khk.ee>
2019-11-05 14:43:20 +0200

    paaris-paaritu Function

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 // function
 
-//defineerimine
-let arv = 5;
-
-//arvutus
-let jaak = arv % 2;
-
-//võrdlus
-
-if(jaak === 0){
-    console.log(arv + ' on paaris')
-} else{
-    console.log(arv + ' on paaritu')
+function evenCheck(arv){
+    let jaak = arv % 2;
+    if(jaak === 0){
+        console.log(arv + ' on paaris')
+    } else{
+        console.log(arv + ' on paaritu')
+    }
 }
+
+evenCheck()
