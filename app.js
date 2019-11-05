0cdfd7fd0ef3e92e9496bfb0d292422104f0ca79
KaurTamm <kaur.tamm@khk.ee>
2019-11-05 15:29:54 +0200

    Function in function UPDATE

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 // function
 
-function pindala(pikkus, laius){
+function ristpindala(pikkus, laius){
     let pindala = pikkus * laius;
     return pindala;
 }
 
-function valjasta(pikkus, laius){
+function valjasta(pikkus = 0, laius = 0){
     console.log('Pikkus = ' + pikkus);
     console.log('Laius = ' + laius);
-    let vastus = pindala(pikkus, laius);
-    console.log('Pindala = ' + vastus);
+    let pindala = ristpindala(pikkus, laius);
+    console.log('Pindala = ' + pindala);
 }
 
 valjasta(5, 5);
+valjasta()
